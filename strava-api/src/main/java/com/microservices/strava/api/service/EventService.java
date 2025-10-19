5a70aab828488d5e78822afdd4b0077922fe1e44
jatinhb <canjatin@gmail.com>
2025-10-19 06:30:00 +0000

    feat: extend module capabilities

---
+++ strava-api/src/main/java/com/microservices/strava/api/service/EventService.java
@@ -0,0 +1,40 @@
+package com.microservices.strava.api.service;
+
+import com.microservices.strava.api.model.Event;
+import com.microservices.strava.api.repository.EventRepository;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MultivaluedMap;
+
+@Stateless
+public class EventService {
+
+    @Inject
+    private javax.enterprise.event.Event<Event> eventSrc;
+
+    @Inject
+    private EventRepository repository;
+
+    private final String API_KEY = "Strava-API-User-Key";
+
+    @Transactional(Transactional.TxType.REQUIRED)
+    public Event save(Event e) {
+        e = repository.create(e);
+        eventSrc.fire(e);
+        return e;
+    }
+
+    /**
+     * 1. Check if API Key exists in header
+     * 2. Check if API Key is valid (pending)
+     * @param headers
+     * @return
+     */
+    public Boolean checkApiKey(HttpHeaders headers) {
+        MultivaluedMap<String, String> map = headers.getRequestHeaders();
+        return map.containsKey(API_KEY);
+    }
+
+}
